00979500b4f2e3bc97e23b50ca4d9746fcfcba8f
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2024-04-17 02:43:41 -0700

    Update video.js

--- video.js
+++ video.js
@@ -9,21 +9,12 @@ const Video = {
     return await Video.start( video )
   },
 
-  async start( element ) {
-    let isStreaming = true 
-    if (navigator.mediaDevices.getUserMedia) {
-      const video = element
-      let stream
-      try {
-        stream = await navigator.mediaDevices.getUserMedia({ video: true })
-      } catch {
-        console.log( 'stream didnt work' )
-        return false
-      }
+
 
       Video.element = video
-      Video.hasPermissions = true
+     // Video.hasPermissions = true
       // note that one is lowercase and we need both!
+ const captureStream = video.captureStream();
       video2.srcObject = stream
       Video.srcObject = stream
       await video2.play()
